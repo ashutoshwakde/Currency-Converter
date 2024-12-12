ff24ea41aa88ea33ca4e6af08d6ab393549290ed
Ashutosh Wakde <wakdeashutosh@gmail.com>
2024-12-12 13:52:39 +0530

    commited

--- src/app/currency-converter/currency-converter.component.ts
+++ src/app/currency-converter/currency-converter.component.ts
@@ -1,34 +1,3 @@
-// import { Component, OnInit } from '@angular/core';
-// import { CurrencyService } from '../Service/currency.service';
-
-// @Component({
-//   selector: 'app-currency-converter',
-//   templateUrl: './currency-converter.component.html',
-//   styleUrls: ['./currency-converter.component.css']
-// })
-// export class CurrencyConverterComponent implements OnInit {
-
-//   fromCurrency: string = 'USD';
-//   toCurrency: string = 'EUR';
-//   amount: number = 1;
-//   convertedAmount: number | null = null;
-
-//   constructor(private currencyService: CurrencyService) { }
-
-//   ngOnInit(): void {
-//   }
-
-//   convertCurrency() {
-//     this.currencyService.getRates(this.fromCurrency).subscribe(data => {
-//       const rate = data.conversion_rates[this.toCurrency];
-//       this.convertedAmount = this.amount * rate;
-//     });
-//   }
-
-// }
-
-
-
 
 import { Component, OnInit } from '@angular/core';
 import { CurrencyService } from '../Service/currency.service';
